8deef13cefcbc51ef63cc14e4efb1731b36e72f5
JosephJanssenGuizona-UCM <josephjanssenguizona1@gmail.com>
2025-08-29 17:09:17 +0800

    Add files via upload

---
+++ chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("I only see my goals, I don't believe in failure");
+        System.out.println("'Cause I know the smallest voices, they can make it major");
+        System.out.println("I got my boys with me, at least those in favor");
+        System.out.println("And if we don't meet before I leave, I hope I'll see you later");
+    }
+}
