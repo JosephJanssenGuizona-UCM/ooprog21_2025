1951f8b1ee6dacc0555284e4fbed2d32cb2f6089
JosephJanssenGuizona-UCM <josephjanssenguizona1@gmail.com>
2025-11-26 14:25:57 +0800

    Add files via upload

---
+++ chapter_7/CompareStrings.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CompareStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name > ");
+        String name = sc.nextLine();
+
+        if (name.equals("Carmen")) {
+            System.out.println("Carmen equals " + name);
+        } else {
+            System.out.println("Carmen does not equal " + name);
+        }
+    }
+}
